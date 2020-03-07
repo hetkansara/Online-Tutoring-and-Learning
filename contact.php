b925dfb5ea65e984016b76053260eb923cb90cee
Kshitija-patel <kshitija.patel.kp@gmail.com>
2020-03-07 15:22:54 -0500

    Design fix

--- contact.php
+++ contact.php
@@ -1,49 +1,51 @@
 <?php require_once "includes/header.php" ?>
 <div>
     <div class="container contact-container">
-        <h2>Contact us</h2>
+        <h2 class="orange-text">Contact us</h2>
         <div class="row">
             <div class="col s12 m12 l8">
                 <p class="contact-description">We are here to answer any questions you may have about our website. Reach out to us and we'll respond as soon as we can.</p>
-                <fieldset>
-                    <p class="form-title">Fill this form:</p>
-                    <div class="row">
-                        <form class="col s12 contact-form">
-                            <div class="row">
-                                <div class="input-field col s6">
-                                    <i class="material-icons prefix">account_circle</i>
-                                    <input id="icon_prefix" type="text" class="validate">
-                                    <label for="icon_prefix">Name</label>
+                <div class="card login-card">
+                    <div class="card-content">
+                        <span class="card-title">Fill this form</span>
+                        <div class="row">
+                            <form class="col s12 contact-form">
+                                <div class="row">
+                                    <div class="input-field col s12 m12 l6">
+                                        <i class="material-icons prefix">account_circle</i>
+                                        <input id="icon_prefix" type="text" class="validate">
+                                        <label for="icon_prefix">Name</label>
+                                    </div>
+                                    <div class="input-field col s12 m12 l6">
+                                        <i class="material-icons prefix">phone</i>
+                                        <input id="icon_telephone" type="tel" class="validate">
+                                        <label for="icon_telephone">Telephone</label>
+                                    </div>
+                                    <div class="input-field col s12 m12 l6">
+                                        <i class="material-icons prefix">email</i>
+                                        <input id="icon_email" type="email" class="validate">
+                                        <label for="icon_email">Email</label>
+                                    </div>
+                                    <div class="input-field col s12 m12 l6">
+                                        <i class="material-icons prefix">subject</i>
+                                        <input id="icon_message" type="text" class="validate">
+                                        <label for="icon_message">Subject</label>
+                                    </div>
+                                    <div class="input-field col s12 m12 l6 contact-message">
+                                        <i class="material-icons prefix">message</i>
+                                        <input id="icon_message" type="text" class="validate">
+                                        <label for="icon_message">Message</label>
+                                    </div>
+                                    <div>
+                                        <button class="btn waves-effect waves-light contact-submit" type="submit" name="action">Submit
+                                            <i class="material-icons right">send</i>
+                                        </button>
+                                    </div>
                                 </div>
-                                <div class="input-field col s6">
-                                    <i class="material-icons prefix">phone</i>
-                                    <input id="icon_telephone" type="tel" class="validate">
-                                    <label for="icon_telephone">Telephone</label>
-                                </div>
-                                <div class="input-field col s6">
-                                    <i class="material-icons prefix">email</i>
-                                    <input id="icon_email" type="email" class="validate">
-                                    <label for="icon_email">Email</label>
-                                </div>
-                                <div class="input-field col s6">
-                                    <i class="material-icons prefix">subject</i>
-                                    <input id="icon_message" type="text" class="validate">
-                                    <label for="icon_message">Subject</label>
-                                </div>
-                                <div class="input-field col s6 contact-message">
-                                    <i class="material-icons prefix">message</i>
-                                    <input id="icon_message" type="text" class="validate">
-                                    <label for="icon_message">Message</label>
-                                </div>
-                                <div>
-                                    <button class="btn waves-effect waves-light contact-submit" type="submit" name="action">Submit
-                                        <i class="material-icons right">send</i>
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
-                </fieldset>
+                </div>
             </div>
             <div class="col s12 m12 l4">
                 <div class="contact-basic">
--- includes/footer.php
+++ includes/footer.php
@@ -1,14 +1,14 @@
 <footer class="page-footer blue-grey">
     <div class="container">
         <div class="row">
-            <div class="col l6 s12">
+            <div class="col s12 m12 l8">
                 <h5 class="white-text">How online tutoring works</h5>
                 <!--<p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
 --><p class="grey-text text-lighten-4">Our Tutors offer to assist you to construct the information and certainty required to form a sense of math, with personal consideration and locks in hands-on examples</p>
 
 
             </div>
-            <div class="col l3 s12 footer-style">
+            <div class="col s12 m12 l4">
                 <h5 class="white-text ">iTutor</h5>
                 <ul>
                     <li><a class="white-text" href="index.php">Home</a></li>
