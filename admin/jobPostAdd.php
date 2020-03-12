649251ac27498a6de5dd8a6e8d4c719b496e06f0
Priyanka Khadilkar <priya@LAPTOP-ETV65Q7J>
2020-03-11 23:24:03 -0400

    added Job Post changes

--- admin/jobPostAdd.php
+++ admin/jobPostAdd.php
@@ -6,31 +6,24 @@
                     <div class="col s12 m12 l8  offset-l2">
                         <div class="card">
                             <div class="card-content">
-                                <span class="card-title">Change Password</span>
+                                <span class="card-title">Add Job Post</span>
                                 <div class="row">
                                     <form class="col s12">
                                         <div class="row margin-bottom-none">
                                             <div class="input-field col s12">
-                                                <i class="material-icons prefix">lock</i>
-                                                <input id="password" type="password" class="validate">
-                                                <label for="password">Old Password</label>
+                                                <input id="title" type="text" class="validate">
+                                                <label for="password">Title</label>
                                             </div>
                                             <div class="input-field col s12">
-                                                <i class="material-icons prefix">lock</i>
-                                                <input id="password" type="password" class="validate">
-                                                <label for="password">New Password</label>
-                                            </div>
-                                            <div class="input-field col s12">
-                                                <i class="material-icons prefix">lock</i>
-                                                <input id="confirmPassword" type="password" class="validate">
-                                                <label for="confirmPassword">Confirm New Password</label>
+                                                <textarea id="textarea2" class="validate materialize-textarea" data-length="120"></textarea>
+                                                <label for="password">Description</label>
                                             </div>
                                             <div class="input-field col s12">
                                                 <button class="btn waves-effect waves-light" type="submit"
                                                         name="action">Submit
                                                 </button>
                                                 <button class="btn waves-effect waves-light" type="submit"
-                                                        name="action">Clear
+                                                        name="action">Back to List
                                                 </button>
                                             </div>
                                     </form>
