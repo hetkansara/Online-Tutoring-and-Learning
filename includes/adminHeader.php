799c03db9e495a1edcc6a6a58b29282d45005929
hetkansara <hetkansara.work@gmail.com>
2020-03-13 23:52:08 -0400

    Link removed

--- includes/adminHeader.php
+++ includes/adminHeader.php
@@ -62,8 +62,7 @@
         <li><a href="mockTests.php"><i class="material-icons">assignment</i>Mock Tests</a></li>
         <li><a href="../admin/jobOpenings.php"><i class="material-icons">work</i>Job Openings</a></li>
         <li><a href="faqList.php"><i class="material-icons">help_outline</i>FAQs</a></li>
-        <li><a href="#!"><i class="material-icons">contact_mail</i>Contact Us</a></li>
-        <li><a href="webContent.php"><i class="material-icons">content_copy</i>Website Content</a></li>
         <li><a href="updateContact.php"><i class="material-icons">contact_mail</i>Contact Us</a></li>
+        <li><a href="webContent.php"><i class="material-icons">content_copy</i>Website Content</a></li>
     </ul>
 </aside>
